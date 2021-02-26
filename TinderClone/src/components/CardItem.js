e3ef78563f45e186344d614f9b7e3b2d75f77b21
Vineet Kumar <kumar8vineet@gmail.com>
2021-02-26 17:02:29 +0530

    App Complete

--- TinderClone/src/components/CardItem.js
+++ TinderClone/src/components/CardItem.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Image, View, Text, TouchableOpacity, Dimensions } from 'react-native';
+import { StyleSheet, Image, View, Text, TouchableOpacity, Dimensions, Animated, } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 FontAwesome.loadFont();
@@ -10,14 +10,50 @@ const DIMENSION_HEIGHT = Dimensions.get("window").height;
 
 const CardItem = ({ Details }) => {
 
-    const navigation = useNavigation()
+    const navigation = useNavigation();
+
+    // const position = new Animated.ValueXY()
+
+    // const likeOpacity = position.x.interpolate({
+    //     inputRange: [(-DIMENSION_WIDTH) / 2, 0, (DIMENSION_WIDTH - 30) / 2],
+    //     outputRange: [0, 0, 1],
+    //     extrapolate: 'clamp'
+    // })
+    // const dislikeOpacity = position.x.interpolate({
+    //     inputRange: [(-DIMENSION_WIDTH) / 2, 0, (DIMENSION_WIDTH) / 2],
+    //     outputRange: [1, 0, 0],
+    //     extrapolate: 'clamp'
+    // })
 
     return (
         <View>
+            {/* <Animated.View style={{
+                transform: [{ rotate: '-30deg' }], position: 'absolute', top: 65, left: 35, zIndex: 1000,
+                opacity: likeOpacity
+            }}>
+                <Text
+                    style={{
+                        borderWidth: 1, borderColor: 'green', color: 'green',
+                        fontSize: 32, fontWeight: '800', padding: 10, borderRadius: 10
+                    }}
+                >LIKE</Text>
+            </Animated.View>
+            <Animated.View style={{
+                transform: [{ rotate: '30deg' }], position: 'absolute', top: 65, right: 35, zIndex: 1000,
+                opacity: dislikeOpacity
+            }}>
+                <Text
+                    style={{
+                        borderWidth: 1, borderColor: 'red', color: 'red',
+                        fontSize: 32, fontWeight: '700', padding: 10, borderRadius: 10
+                    }}
+                >NOPE</Text>
+            </Animated.View> */}
             <Image
                 source={Details.pic}
                 style={styles.image}
             />
+
             <View style={styles.textContainer}>
                 <View style={styles.textRow}>
                     <TouchableOpacity
